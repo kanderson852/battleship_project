68621e2a25b7f8c699dc27905e5757830948bf81
Mallory Vining <mallorygallery12@gmail.com>
2021-10-19 18:17:15 -0600

    feat: add place_ship method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,5 +1,20 @@
 class Cell
+  attr_reader :coordinate,
+              :ship,
+              :is_empty
   def initialize(coordinate)
-
+    @coordinate = coordinate
+    @ship = nil
+    @is_empty = true
   end
+
+  def empty?
+    @is_empty
+  end
+
+  def place_ship(ship)
+    @ship = ship
+    @is_empty = false
+  end
+
 end
