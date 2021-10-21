5c751491a029b78aa21b1db3443a81d07f2d39ae
Kelly Anderson <kean9528@colorado.edu>
2021-10-20 21:01:58 -0600

    feat: add method for coordinates

--- lib/board.rb
+++ lib/board.rb
@@ -22,4 +22,15 @@ class Board
                "D4" => Cell.new("D4")}
   end
 
+  def valid_coordinate?(coordinate)
+    @cells.keys.include?(coordinate)
+  end
+
+  def valid_placement?(ship, coordinates)
+    if ship.length == coordinates.length
+      true
+    else
+      false
+    end
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -22,4 +22,26 @@ RSpec.describe Board do
   #   cell_2 = Cell.new("C3")
   #   expect(board.cells).to eq()
   # end
+
+  it 'validates coordinates' do
+    board = Board.new
+    expect(board.valid_coordinate?("A1")).to eq(true)
+    expect(board.valid_coordinate?("D4")).to eq(true)
+    expect(board.valid_coordinate?("A5")).to eq(false)
+    expect(board.valid_coordinate?("E1")).to eq(false)
+    expect(board.valid_coordinate?("A22")).to eq(false)
+  end
+
+  it 'validates placements' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq (false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+  it 'validates placements' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
 end
