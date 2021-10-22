054b2ffc367733389140681f3daaf3507a9facef
Kelly Anderson <kean9528@colorado.edu>
2021-10-21 19:26:48 -0600

    test: create tests for vertical and horizontal placements

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,12 +16,12 @@ RSpec.describe Board do
     expect(board.cells).to be_a(Hash)
   end
 
-  # it "cells" do
-  #   board = Board.new
-  #   cell_1 = Cell.new("B4")
-  #   cell_2 = Cell.new("C3")
-  #   expect(board.cells).to eq()
-  # end
+  it "cells" do
+    board = Board.new
+    cell_1 = Cell.new("B4")
+    cell_2 = Cell.new("C3")
+    expect(board.cells).to match(hash_including("A1","A2","A3","A4","B1","B2","B3","B4","C1","C2","C3","C4","D1","D2","D3","D4"))
+  end
 
   it 'validates coordinates' do
     board = Board.new
@@ -32,16 +32,29 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
 
-  it 'validates placements' do
+  xit 'validates placements' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq (false)
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
   end
-  it 'validates placements' do
+  xit 'validates placements' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+  end
+
+  it 'checks vertical placement' do
+    board = Board.new
+    expect(board.vertical(["A1","A2","A3"])).to eq(1)
+    expect(board.vertical(["A1","B2","C3"])).to eq(3)
+  end
+
+  it 'checks horizontal placement' do
+    board = Board.new
+    expect(board.horizontal(["A1","A2","A3"])).to eq(3)
+    expect(board.horizontal(["A1","B1","C1"])).to eq(1)
+  end
 end
