164d3cba4f6446a8b6c010ae51eb3df08b23fe56
Kelly Anderson <kean9528@colorado.edu>
2021-10-24 19:20:46 -0600

    test: test computer user placements

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -17,4 +17,14 @@ describe Turn do
     expect(turn.computer_ships).to eq([])
     expect(turn.user_ships).to eq([])
   end
+
+  it 'computer places ships' do
+    turn = Turn.new
+    expect(turn.computer_place_ships).to eq(turn.computer_board.render(true))
+  end
+
+  it 'user places ships' do
+    turn = Turn.new
+    expect(turn.user_place_ships).to eq(turn.user_board.render(true))
+  end
 end
