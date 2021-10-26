29131bd26114a3330ef1707b450b39ada0871e3f
Mallory Vining <mallorygallery12@gmail.com>
2021-10-26 11:47:41 -0600

    feat: Condense player board

--- lib/player.rb
+++ lib/player.rb
@@ -2,54 +2,51 @@ require './lib/board'
 require "./lib/ship"
 require "./lib/cell"
 class Player
-  attr_reader :computer_board, :user_board
-  def initialize()
-    @computer_board = Board.new
-    @user_board = Board.new
+  attr_reader :board
+  def initialize
+    @board = Board.new
   end
 
   def random_placer_cruiser
-    @computer_board.cells.keys.to_a.sample(3)
+    @board.cells.keys.to_a.sample(3)
   end
 
   def random_placer_submarine
-    @computer_board.cells.keys.to_a.sample(2)
+    @board.cells.keys.to_a.sample(2)
   end
 
   def computer_place_ships
     cruiser = Ship.new("Cruiser", 3)
-    until @computer_board.valid_placement?(cruiser, coordinates = random_placer_cruiser) == true do
+    until @board.valid_placement?(cruiser, coordinates = random_placer_cruiser) == true do
       random_placer_cruiser
     end
-    @computer_board.place_ship(cruiser, coordinates)
+    @board.place_ship(cruiser, coordinates)
     submarine = Ship.new("Submarine", 2)
-    until @computer_board.valid_placement?(submarine, coordinates = random_placer_submarine) == true do
+    until @board.valid_placement?(submarine, coordinates = random_placer_submarine) == true do
       random_placer_submarine
     end
-    @computer_board.place_ship(submarine, coordinates)
-    @computer_board.render(true)
+    @board.place_ship(submarine, coordinates)
+    @board.render(true)
   end
 
   def user_place_ships
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     coordinates = []
-    until @user_board.valid_placement?(cruiser, coordinates) == true do
+    until @board.valid_placement?(cruiser, coordinates) == true do
       coordinates = gets.chomp.split(' ')
-      if @user_board.valid_placement?(cruiser, coordinates) == false
+      if @board.valid_placement?(cruiser, coordinates) == false
         puts "Those are invalid coordinates. Please try again:"
       end
     end
-    @user_board.place_ship(cruiser, coordinates)
-    until @user_board.valid_placement?(submarine, coordinates) == true do
+    @board.place_ship(cruiser, coordinates)
+    until @board.valid_placement?(submarine, coordinates) == true do
       coordinates = gets.chomp.split(' ')
-      if @user_board.valid_placement?(submarine, coordinates) == false
+      if @board.valid_placement?(submarine, coordinates) == false
         puts "Those are invalid coordinates. Please try again:"
       end
     end
-    @user_board.place_ship(submarine, coordinates)
-    @user_board.render(true)
+    @board.place_ship(submarine, coordinates)
+    @board.render(true)
   end
 end
-# player = Player.new
-# player.user_place_ships
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -12,16 +12,11 @@ describe Player do
 
   it 'attributes' do
     player = Player.new
-    expect(player.computer_board).to be_an_instance_of(Board)
+    expect(player.board).to be_an_instance_of(Board)
   end
 
   it 'computer places ships' do
     player = Player.new
-    expect(player.computer_place_ships).to eq(player.computer_board.render(true))
+    expect(player.computer_place_ships).to eq(player.board.render(true))
   end
-
-  # it 'user places ships' do
-  #   player = Player.new
-  #   expect(player.user_place_ships).to eq()
-  # end
 end
