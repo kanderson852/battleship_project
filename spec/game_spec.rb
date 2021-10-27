37b9098c4b31196c8b753c77c3764940072d6541
Mallory Vining <mallorygallery12@gmail.com>
2021-10-27 11:53:14 -0600

    feat: Refactor

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,27 +7,19 @@ require "./lib/game"
 
 describe Game do
   it 'exists' do
-    user = Player.new
-    computer = Player.new
-    game = Game.new(computer, user)
+    game = Game.new
     expect(game).to be_an_instance_of(Game)
   end
 
   it 'attributes' do
-    user = Player.new
-    computer = Player.new
-    game = Game.new(computer, user)
+    game = Game.new
     expect(game.computer).to be_an_instance_of(Player)
     expect(game.user).to be_an_instance_of(Player)
-
   end
 
-  it 'has a ships sunk count' do
-    user = Player.new
-    computer = Player.new
-    game = Game.new(computer, user)
-
-    expect(game.player_ships_sunk).to eq(0)
-    expect(game.computer_ships_sunk).to eq(0)
+  it "#valid_fire" do
+    game = Game.new
+    expect(game.valid_fire?("B3")).to eq(true)
+    expect(game.valid_fire?("Z9")).to eq(false)
   end
 end
